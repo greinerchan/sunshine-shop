4aa8c6ab7c8c2aa718d207f23f8e96af039be51a
greinerchan <greinerchan@gmail.com>
2020-10-16 11:39:01 -0400

    fix complicate querry issue by using native sql in jpa repository, now spring boot back end is ready

--- sunshine-shop/sunshine_shop_springboot/src/main/java/com/perscholas/case_study/sunshine_shop/dao/ProductCategoryRepository.java
+++ sunshine-shop/sunshine_shop_springboot/src/main/java/com/perscholas/case_study/sunshine_shop/dao/ProductCategoryRepository.java
@@ -1,27 +1,26 @@
 package com.perscholas.case_study.sunshine_shop.dao;
 
-import com.perscholas.case_study.sunshine_shop.entity.Product;
 import com.perscholas.case_study.sunshine_shop.entity.ProductCategory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 @CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
     Page<ProductCategory> findById(@RequestParam("id") Long id, Pageable pageable);
 
-    @Query("From ProductCategory AS PC INNER JOIN PC.productSubCategories AS PSC " +
-            "INNER JOIN PSC.products AS PD WHERE PD.id = ?1")
-    List<Product> FindAllWithDescriptionQuery(Long id);
-
+//    @Query("From ProductCategory AS PC INNER JOIN PC.productSubCategories AS PSC " +
+//            "INNER JOIN PSC.products AS PD WHERE PD.id = ?1")
+//    List<Product> FindAllWithDescriptionQuery(Long id);
 }
+
+
+
+
 
 
 
--- sunshine-shop/sunshine_shop_springboot/src/main/java/com/perscholas/case_study/sunshine_shop/dao/ProductRepository.java
+++ sunshine-shop/sunshine_shop_springboot/src/main/java/com/perscholas/case_study/sunshine_shop/dao/ProductRepository.java
@@ -4,11 +4,20 @@ import com.perscholas.case_study.sunshine_shop.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByProductSubCategoryId(@RequestParam("id") Long id, Pageable pageable);
+
+    @Query(value = "SELECT * from product inner join sub_category sc on product.sub_category_id = sc.id " +
+            "inner join category c on c.id = sc.category_id where c.id =:id", nativeQuery = true)
+    public List<Product> FindAllWithDescriptionQuery(@Param("id") Long id);
 }
+
 
